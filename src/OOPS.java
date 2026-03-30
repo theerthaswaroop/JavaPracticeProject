bf14c8f61b8a66b39888bb3fd821db8200c6fe3b
theerthaswaroop <swaroopn414@gmail.com>
2026-03-30 12:14:08 +0530

    Inheritence

--- src/OOPS.java
+++ src/OOPS.java
@@ -1,83 +1,27 @@
-class Pen {
-    String color;
-    String type;//ballpoint;gel
-
-    public void write() {
-        System.out.println("Write Something");
+class Shape {
+    public void area() {
+        System.out.println("displays area");
     }
 
-    public void printColor() {
-        System.out.println(this.color);
-    }
-
-
+    ;
 }
 
-class Student {
-    String name;
-    int age;
-
-    public void printInfo() {
-        System.out.println(this.age);
-        System.out.println(this.name);
-    }
-
-    // normal constructor
-    Student() {
-        System.out.println("Constructor Called");
-    }
-
-    // parameterized constructor
-    Student(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    // copy constructor; assigning values of a different constructor to the current constructor;
-
-    Student(Student s2) {
-        this.name = s2.name;
-        this.age = s2.age;
+// Single level Inheritence
+class Triangle extends Shape {
+    public void area(int l, int h) {
+        System.out.println(1 / 2 * l * h);
     }
 }
 
+class EquilateralTriangel extends Triangle {
+    public void ara(int l, int h) {
+        System.out.println(1 / 2 * l * h);
+    }
 
-public class OOPS {
-    public static void main(String args[]) {
-        Pen pen1;
-        pen1 = new Pen(); // one way to create an object
-
-        Pen pen2 = new Pen(); // second way to create an object
-
-        pen1.color = "red";
-        pen2.color = "black";
-
-        pen2.type = "Gel";
-
-        pen1.write();
-
-        pen2.printColor();
-        pen1.printColor();
-
-        Student s1 = new Student();
-        s1.age = 24;
-        s1.name = "Michael";
-
-        Student s2 = new Student("hahr", 21);
-        s2.printInfo();
-        s2.age = 25;
-        s2.name = "Kareem";
-
-        s1.printInfo();
-        s2.printInfo();
-
-
-        s1.name = "Swaroop";
-        s2.age = 24;
-
-        Student s3 = new Student(s1);
-        s3.printInfo();
-
-
+    class Circle extends Shape {
+        public void area(int r) {
+            System.out.println(3.14 * r * r);
+        }
     }
 }
+

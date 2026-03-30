f1e3714ccc7b677a923c59e6f56054c731f0add8
theerthaswaroop <swaroopn414@gmail.com>
2026-03-30 10:05:17 +0530

    Java OOPS telusko example

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Keyboard {
+    int keys = 9;
+    String color;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public void pressed() {
+        System.out.println("Signal sent");
+    }
+
+    public void throwIt() {
+        System.out.println("Throwing it");
+        keys--;
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+
+        Keyboard obj;
+        obj = new Keyboard();
+
+        obj.throwIt();
+        obj.pressed();
+
+        Keyboard obj2;
+        obj2 = new Keyboard();
+
+        obj2.throwIt();
+
+        System.out.println(obj.keys);
     }
 }

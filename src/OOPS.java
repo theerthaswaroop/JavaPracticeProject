c7ae9eb3be28eba94f8c3e60e456cc318c2169f1
theerthaswaroop <swaroopn414@gmail.com>
2026-03-30 11:23:20 +0530

    Constructor and It's types

--- src/OOPS.java
+++ src/OOPS.java
@@ -21,6 +21,24 @@ class Student {
         System.out.println(this.age);
         System.out.println(this.name);
     }
+
+    // normal constructor
+    Student() {
+        System.out.println("Constructor Called");
+    }
+
+    // parameterized constructor
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // copy constructor; assigning values of a different constructor to the current constructor;
+
+    Student(Student s2) {
+        this.name = s2.name;
+        this.age = s2.age;
+    }
 }
 
 
@@ -45,12 +63,20 @@ public class OOPS {
         s1.age = 24;
         s1.name = "Michael";
 
-        Student s2 = new Student();
+        Student s2 = new Student("hahr", 21);
+        s2.printInfo();
         s2.age = 25;
         s2.name = "Kareem";
 
         s1.printInfo();
         s2.printInfo();
+
+
+        s1.name = "Swaroop";
+        s2.age = 24;
+
+        Student s3 = new Student(s1);
+        s3.printInfo();
 
 
     }

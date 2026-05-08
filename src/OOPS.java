5e3c1369b69f0623651576da3d2cfce5aa092a42
theerthaswaroop <swaroopn414@gmail.com>
2026-05-08 12:53:41 +0530

    interfaces example

--- src/OOPS.java
+++ src/OOPS.java
@@ -1,27 +1,12 @@
-abstract class Animal { //
-    abstract void walk();
-
-    Animal() {
-        System.out.println("You are creating a new animal");
-    }
+interface Animal {
+    public void walk();
 }
 
-class Horse extends Animal {
+class Horse implements Animal {
     public void walk() {
-        System.out.println("walks on 4 legs");
-    }
-
-    Horse() {
-        System.out.println("You are creating a new horse");
+        System.out.println("HORSE");
     }
 }
-
-class Chicken extends Animal {
-    public void walk() {
-        System.out.println("wlaks on 2 legs");
-    }
-}
-
 
 public class OOPS {
     public static void main(String args[]) {
